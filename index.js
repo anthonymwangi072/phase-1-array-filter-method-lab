a7244accf89be1cf0f128f36044fe896e3113c36
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-08-21 12:06:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+// function findMatching(drivers, str){
+//     return drivers.filter(driver => str.tolowerCase() === driver.tolowerCase());
+
+// }
+function findMatching(drivers,array) {
+    return drivers.filter(user => array.toLowerCase() === user.toLowerCase());
+  }
+
+function fuzzyMatch(drivers,letter) {
+    return drivers.filter(user => user.startsWith(letter));
+    }
+    
+    function matchName (drivers,name) {
+    return drivers.filter(user => user.name === name);
+    }
